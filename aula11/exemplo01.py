cd4479a20e5bfed411d26ec9d8c205ed39802402
gustavosenac25 <gustavo.ocosta@aluno.senac.rio>
2026-01-06 19:15:43 -0300

    random números decimais

--- aula11/exemplo01.py
+++ aula11/exemplo01.py
@@ -4,4 +4,12 @@ import random
 #random.rendit -> Números inteiros
 n = random.randint(1, 10)
 m = random.randint(1, 10)
-print(n, m)
+# print(n, m)
+
+#radom.uniform -> Números decimais
+n_decimal = random.uniform(1, 10)
+print (n_decimal)
+print (f'{n_decimal:.2f}') #formata o número
+
+numero_decimal = round(n_decimal, 2) #arredonda o número "2" significa duas casas decimais
+print (numero_decimal)
