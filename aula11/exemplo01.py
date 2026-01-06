15acba3ef61d3bbed231698a81114082e6de7b9c
gustavosenac25 <gustavo.ocosta@aluno.senac.rio>
2026-01-06 20:51:14 -0300

    exemplo 02

--- aula11/exemplo01.py
+++ aula11/exemplo01.py
@@ -1,17 +1,21 @@
 #bibliotecas
 import random
+import os
 
 #random.rendit -> Números inteiros
-n = random.randint(1, 10)
-m = random.randint(1, 10)
-# print(n, m)
+# n = random.randint(1, 10)
+# m = random.randint(1, 10)
+# # print(n, m)
 
-#radom.uniform -> Números decimais
-n_decimal = random.uniform(1, 10)
-print (n_decimal)
-print (f'{n_decimal:.2f}') #formata o número
+# #radom.uniform -> Números decimais
+# n_decimal = random.uniform(1, 10)
+# print (n_decimal)
+# print (f'{n_decimal:.2f}') #formata o número
 
-numero_decimal = round(n_decimal, 2) #arredonda o número "2" significa duas casas decimais
-print (numero_decimal)
+# numero_decimal = round(n_decimal, 2) #arredonda o número "2" significa duas casas decimais
+# print (numero_decimal)
 
-#Para salvar outra vez
+#Gerar números aleatorios e criar uma lista (for) com x (range) itens
+lst_numeros = [random.randint(1, 10) for num in range (5)]
+os.system('cls') #Limpa a tela antes de printar
+print(lst_numeros)
